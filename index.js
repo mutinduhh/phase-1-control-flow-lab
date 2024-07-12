d302031f04788da840831f4f04e0d72104c940f1
mutinduhh <jonathan.mutinda@student.moringaschool.com>
2024-07-12 08:15:31 +0300

    Answer

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(somevalue){
+  if (somevalue<= 400) {return 'This one is on me!';}
+  if (400<somevalue && somevalue<2000) {return 'That will be twenty bucks.';}
+  if ( somevalue>2500) {return  'No can do.';}else {return 'I will gladly take your thirty bucks.';}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC"? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+    switch (tip) {
+      case 'generous':
+          return 'Thank you so much.';
+      case 'not as generous':
+          return 'Thank you.';
+      case 'thanks for everything':
+          return 'Bye.';
+    }
+  }
